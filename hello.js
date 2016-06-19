e352eaab88e5fd7e36f21f958ee1e859401a97e2
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-18 23:47:38 -0400

    use settimeout

--- hello.js
+++ hello.js
@@ -1,9 +1,26 @@
+// var http = require('http');
+//
+// http.createServer(function(request, response) {
+//   response.writeHead(200);
+//   response.write('Hello, this is dog.');
+//   response.end();
+// }).listen(3000);
+//
+// console.log('listening on port 3000');
+
+
 var http = require('http');
 
+//request event
 http.createServer(function(request, response) {
   response.writeHead(200);
-  response.write('Hello, this is dog.');
-  response.end();
+  response.write('dog is running.');
+  //timeout event
+  setTimeout(function() {
+    response.write('dog is done.');
+    response.end();
+  }, 5000);
+
 }).listen(3000);
 
 console.log('listening on port 3000');
