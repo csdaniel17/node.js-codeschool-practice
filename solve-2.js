701f082085955a4f236cd995874d64505ba48375
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-21 08:59:33 -0400

    comment code

--- solve-2.js
+++ solve-2.js
@@ -7,14 +7,18 @@ function solveRect(l, b) {
   //3rd parameter is callback function
   rect(l, b, function(err, rectangle) {
     if (err) {
+      //print out error message from rectangle-2.js
       console.log(err);
     } else {
+      //if error is null, then you got back the correct JS object
+      //will print out area and perimeter
       console.log('The area of a rectangle of dimensions length = ' + l + ' and breadth = ' + b + ' is ' + rectangle.area());
       console.log('The perimeter of a rectangle of dimensions length = ' + l + ' and breadth = ' + b + ' is ' + rectangle.perimeter());
     }
   });
 }
 
+//to test
 solveRect(2, 4);
 solveRect(3, 5);
 solveRect(-3, 5);
